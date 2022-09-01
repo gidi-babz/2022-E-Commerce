767d2f56f449966546e271b89e38110fb6079d70
Gidi Babz <tunex4ind@gmail.com>
2022-09-01 18:16:31 +0100

    products details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
 	AiOutlineMinus,
 	AiOutlinePlus,
 	AiFillStar,
 	AiOutlineStar,
 } from 'react-icons/ai';
+import { Product } from '../../components';
 
 import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = ({ product, products }) => {
 	const { image, name, details, price } = product;
 
+	const [index, setIndex] = useState(0);
+
 	return (
 		<div>
 			<div className="product-detail-container">
 				<div>
 					<div className="image-container">
-						<img src={urlFor(image && image[0])} alt={name} />
+						<img
+							src={urlFor(image && image[index])}
+							alt={name}
+							className="product-detail-image"
+						/>
 					</div>
-					{/* <div className="small-images-container">
-            {image?.map((item, i) => (
-              <img src={urlFor(item)} className=""
-                onMouseEnter=""
-              />
-            ))}
-          </div> */}
+					<div className="small-images-container">
+						{image?.map((item, i) => (
+							<img
+								src={urlFor(item)}
+								className={
+									i === index ? 'small-image selected-image' : 'small-image'
+								}
+								onMouseEnter={() => setIndex(i)}
+							/>
+						))}
+					</div>
 				</div>
 				<div className="product-detail-desc">
 					<h1>{name}</h1>
@@ -63,6 +75,17 @@ const ProductDetails = ({ product, products }) => {
 						<button onClick="" type="button" className="buy-now">
 							Buy Now
 						</button>
+					</div>
+				</div>
+			</div>
+
+			<div className="maylike-products-wrapper">
+				<h2>You may also like</h2>
+				<div className="marquee">
+					<div className="maylike-products-container track">
+						{products?.map((item) => (
+							<Product key={item._id} product={item} />
+						))}
 					</div>
 				</div>
 			</div>
